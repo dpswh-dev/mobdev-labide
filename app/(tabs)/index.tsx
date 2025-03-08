5b854b33b7b91132e3be5356c76ca2238cef4cbc
dpswh-dev <lanutanenzo.developer@gmail.com>
2025-03-08 20:28:18 +0800

    feat: add  page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,42 +1,77 @@
-import {
-  Image,
-  StyleSheet,
-  Platform,
-  Button,
-  View,
-  TouchableOpacity,
-  Text,
-} from "react-native";
+import { Image, StyleSheet, View } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [emailText, setEmailText] = useState("");
+  const [passwordText, setPasswordText] = useState("");
+
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => alert("Hello")} style={styles.button}>
-        <Text style={styles.buttonText}>Hello</Text>
-      </TouchableOpacity>
+    <View style={styles.mainContainer}>
+      <View style={styles.contentContainer}>
+        <Image
+          source={require("../../assets/images/laravel-nobg.png")}
+          resizeMode="contain"
+          style={{ marginHorizontal: "auto", width: 200, height: 200 }}
+        />
+        <TextInput
+          label="Email"
+          value={emailText}
+          onChangeText={setEmailText}
+          mode="outlined"
+          style={styles.textInput}
+        />
+        <TextInput
+          label="Password"
+          value={passwordText}
+          onChangeText={setPasswordText}
+          mode="outlined"
+          style={styles.textInput}
+        />
+        <Button
+          style={styles.button}
+          mode="contained"
+          onPress={() =>
+            alert(
+              JSON.stringify({
+                email: emailText,
+                password: passwordText,
+              })
+            )
+          }
+        >
+          Sign in
+        </Button>
+        <Button
+          style={styles.button}
+          mode="outlined"
+          onPress={() => alert("Pressed")}
+        >
+          Sign up
+        </Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
+    flexDirection: "column",
+    gap: 12,
+  },
+  mainContainer: {
     flex: 1,
+    padding: 24,
+    backgroundColor: "#fff",
+    flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
+  },
+  textInput: {
+    borderRadius: 100,
   },
   button: {
-    backgroundColor: "#64558F",
-    padding: 10,
     borderRadius: 5,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
+    paddingVertical: 5,
   },
 });
